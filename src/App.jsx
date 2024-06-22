eca7507b7a14fbb05e70f51e147cad251f7ff9bf
rohit3110-pro <mudakuderohit@gmail.com>
2024-06-22 20:29:43 +0530

    create header components

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import Signin from './pages/signin';
 import Signout from './pages/signout';
 import Profile from "./pages/Profile";
 import About from "./pages/about";
+import Header from "./components/Header";
 
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<Signin />} />
--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function signin() {
   return (
-    <div>signin</div>
+    <div>SignIn</div>
   )
 }
