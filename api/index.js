7c4848bb61312dbec37a50d0db89251aba60adc6
rohit3110-pro <mudakuderohit@gmail.com>
2024-06-26 14:48:11 +0530

    create a middleware and a function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -23,3 +23,14 @@ app.use('/api/auth', authRouter); // Add the leading slash
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message,
+    });
+
+});
