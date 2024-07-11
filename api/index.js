b8a5ebb54281ecc2c4713b41baf2427563bc850b
rohit3110-pro <mudakuderohit@gmail.com>
2024-07-11 19:51:18 +0530

    add create listing api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser'; // Import cookie-parser
 
 dotenv.config();
@@ -21,6 +22,7 @@ app.use(cookieParser()); // Middleware to parse cookies
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing',listingRouter);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
